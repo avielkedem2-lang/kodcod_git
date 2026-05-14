4ef9b0b7df7202429a8211678d5be39dc1d16dbf
אביאל קדם <avielkedem2@gmail.com>
2026-05-14 12:04:49 +0300

    I improved the game

--- week5/Basic_Python/project_hanged_man/guess_the_word.py
+++ week5/Basic_Python/project_hanged_man/guess_the_word.py
@@ -6,18 +6,19 @@ print("")
 print("")
 
 
-def choosing_word():
+def get_word_from_the_bank():
     word_bank = [
     "Galaxy", "Python", "Mountain", "River", "Keyboard", 
     "Ocean", "Software", "Cloud", "Algorithm", "Coffee", 
     "Forest", "Sunset", "Database", "Laptop", "Science"]
     word = random.choice(word_bank)
+    word = "aviel"
     return word
 
 
 
 
-def status_word(list_words, word):
+def result_guess(list_words, word):
     print_word = ""
     for char in word.lower():
         if char in list_words:
@@ -28,35 +29,65 @@ def status_word(list_words, word):
 
 
 
-the_guess_word = choosing_word()
-guesses = 8
-list_words = []
-flag_game_on = True
-def show_status_game(status_word, the_guess_word, guesses, list_words):
-    print(f"Word:{status_word(list_words, the_guess_word)}")
+
+def show_status_game(the_guess_word, guesses, list_words):
+    print(f"Word:{result_guess(list_words, the_guess_word)}")
     print(f"You have a {guesses} guesses!")
     print(f"You already guessed: {list_words}")
     print("")
     print("")
     print("")
 
-while flag_game_on:
-    show_status_game(status_word, the_guess_word, guesses, list_words)
-    choice = input("Choose a letter: ")
-    list_words.append(choice)
-    if choice in the_guess_word:
-        print("wonderful!!!")
-        checke_guess = status_word(list_words, the_guess_word)
-        if "_" not in checke_guess:
-            print(f"Congratulations, you found the word: {the_guess_word}")
+
+
+def is_cher_in_word(user_choice, the_guess_word, my_bank_words):
+    global guesses
+    if user_choice.lower() in the_guess_word.lower():
+        print("wonderful!!! \U0001F600")
+        checke_guess = result_guess(my_bank_words, the_guess_word)
+        if "_" not in checke_guess or user_choice.lower() == the_guess_word.lower():
+            print(f"Congratulations, you found the word: {the_guess_word} \U0001F3AF")
             flag_game_on =False 
+            return flag_game_on
+        else:
+            flag_game_on = True
+            return flag_game_on
 
     else:
         guesses -= 1
-        print("You guessed wrong!")
+        print("You guessed wrong! \u274C")
         if guesses == 0:
-            print("You're out of guesses. You're disqualified!!!")
+            print("You're out of guesses. You're disqualified!!! \U0001F480")
             flag_game_on = False
+            return flag_game_on
+        else:
+            flag_game_on = True
+            return flag_game_on
+
+
+
+
+
+the_guess_word = get_word_from_the_bank()
+guesses = 8
+my_bank_words = []
+flag_game_on = True
+
+
+
+while flag_game_on:
+    show_status_game(the_guess_word, guesses, my_bank_words)
+
+    user_choice = input("Choose a letter or the word: ")
+    if not user_choice.isalpha():
+        print("you need to input letter or word!")
+        continue
+    
+    if user_choice.lower() not in my_bank_words:
+        my_bank_words.append(user_choice.lower())
+    
+        flag_game_on = is_cher_in_word(user_choice, the_guess_word, my_bank_words)
+    
             
 
         
